564501e7f1e26ca4835edb942b25d9c7d77b9104
sewright22 <sewright22@gmail.com>
2015-02-16 06:26:05 -0600

    Working on drag and drop

--- app/src/main/java/com/projects/sequence/GameActivity.java
+++ app/src/main/java/com/projects/sequence/GameActivity.java
@@ -1,11 +1,17 @@
 package com.projects.sequence;
 
+import android.content.ClipData;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -20,7 +26,8 @@ public class GameActivity extends ActionBarActivity
         setContentView(R.layout.activity_game);
         game = new GameBoard();
         //TextView text = (TextView)findViewById(R.id.textView2);
-
+        findViewById(R.id.blueChip).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.topLeft).setOnDragListener(new MyDragListener());
     }
 
     public int GetGameBoardSquareColor(View view)
@@ -53,4 +60,61 @@ public class GameActivity extends ActionBarActivity
 
         return super.onOptionsItemSelected(item);
     }
+
+    private final class MyTouchListener implements View.OnTouchListener
+    {
+        public boolean onTouch(View view, MotionEvent motionEvent)
+        {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+            {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class MyDragListener implements View.OnDragListener
+    {
+        Drawable enterShape = getResources().getDrawable(R.drawable.spades_10_150);
+        Drawable normalShape = getResources().getDrawable(R.drawable.spades_10_150);
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    v.setBackgroundDrawable(enterShape);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    v.setBackgroundDrawable(normalShape);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign View to ViewGroup
+                    View view = (View) event.getLocalState();
+                    ViewGroup owner = (ViewGroup) view.getParent();
+                    owner.removeView(view);
+                    LinearLayout container = (LinearLayout) v;
+                    container.addView(view);
+                    view.setVisibility(View.VISIBLE);
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    v.setBackgroundDrawable(normalShape);
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
 }
+
+
